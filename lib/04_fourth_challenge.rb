d9590370c9e305b337328e4e48dab9a57f845f9f
Alan sanchez <asanchezhaza@gmail.com>
2020-10-18 04:53:46 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -1,25 +1,5 @@
 def fourth_challenge
-  epic_tragedy = {
-   :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"},
-      :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [
-       {name: "Benvolio", age: "17", attitute: "worried"},
-       {name: "Mercutio", age: "18", attitute: "hot-headed"}
-      ]
-   }, 
-   :capulet => {
-      :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
-       {name: "Steven", age: "30", attitute: "confused"},
-       {name: "Nurse", age: "44", attitute: "worried"}
-      ]
-   }
-}
-
+  
   
 
 end
